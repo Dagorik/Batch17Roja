8da562ba7b7997313f8400099661433bb07cc97e
Dagorik <dagorik@gmail.com>
2017-10-31 21:29:55 -0600

    lista

--- lista.py
+++ lista.py
@@ -12,14 +12,51 @@ for i in list3:
 	print(i)
 '''
 
-frutas = ['naranja','manzana','pera','fresa','banana','manzana','kiwi']
+frutas = ['naranja','manzana','pera','fresa',
+'banana','manzana','kiwi']
 print(frutas)
 
 frutas.append('uva')
+print("APPEND")
 print(frutas)
 
-frutas.extend(list2)
-print(frutas)
+#frutas.extend(list2)
+#print(frutas)
 
 frutas.insert(0,'melon')
+print("INSERT1")
 print(frutas)
+
+
+frutas.pop(3)
+print("POP")
+print(frutas)
+
+
+frutas.remove("manzana")
+print("REMOVE")
+print(frutas)
+
+
+frutas.reverse()
+print("REVERSE")
+print(frutas)
+
+
+frutas.sort(reverse = True)
+print("SORT")
+print(frutas)
+
+print("COUNT")
+print(frutas.count('manzana'))
+
+print("INDEX")
+print(frutas.index('naranja',0))
+
+
+for i in frutas:
+	if i == "manzana":
+		print("MANZANA")
+
+
+
